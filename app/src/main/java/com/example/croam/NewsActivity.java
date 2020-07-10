041b803b733a4b652cd61655e8bb9b1a59f4ade9
Shubham Kumar <shubhamcricketer.kumar27@gmail.com>
2020-07-10 12:57:38 +0530

    partial news implemented

--- app/src/main/java/com/example/croam/NewsActivity.java
+++ app/src/main/java/com/example/croam/NewsActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,19 +17,24 @@ import java.util.Map;
 import java.util.Objects;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class NewsActivity extends AppCompatActivity {
-
+public class NewsActivity extends AppCompatActivity  implements NewsAdapter.OnItemListener{
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     ArrayList<ReportModel> reportsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        mRecyclerView = findViewById(R.id.newsRecyclerView);
         reportsList = new ArrayList<>();
 
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(
@@ -37,6 +43,19 @@ public class NewsActivity extends AppCompatActivity {
         final Map<String, String> auth = new HashMap<>();
         auth.put("Authorization",
                 "Token " + token);
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        //  mRecyclerView.addItemDecoration(new DividerItemDecoration(mRecyclerView.getContext(), DividerItemDecoration.VERTICAL));
+
+        // specify an adapter and pass in our data model list
+
+        mAdapter = new NewsAdapter(reportsList, this,this);
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
 
         Call<ResponseBody> call = MyApi.Companion.invoke().getReports(auth);
         Objects.requireNonNull(call).enqueue(new Callback<ResponseBody>() {
@@ -58,6 +77,7 @@ public class NewsActivity extends AppCompatActivity {
                                         report.getString("country"), report.getString("state"),
                                         report.getString("city"), report.getString("description"),
                                         report.getString("created")));
+                        mAdapter.notifyDataSetChanged();
 
                     }
 //                    {"id":1,"name":"New","media":"https:\/\/storage.googleapis
@@ -76,6 +96,26 @@ public class NewsActivity extends AppCompatActivity {
                 Log.e("news", t.getMessage());
             }
         });
+
+    }
+
+    @Override
+    public void OnBookClick(int position) {
+
+    }
+
+    @Override
+    public void OnBookLongClick(int position, View view) {
+
+    }
+
+    @Override
+    public void OnButton1Click(int position, View view) {
+
+    }
+
+    @Override
+    public void OnButton2Click(int position, View view) {
 
     }
 }
