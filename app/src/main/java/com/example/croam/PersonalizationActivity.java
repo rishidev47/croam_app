53228b586faa281531856cb7ab06a1e6a69ed36b
Shubham Kumar <shubhamcricketer.kumar27@gmail.com>
2020-07-16 17:45:56 +0530

    personalization partial

---
+++ app/src/main/java/com/example/croam/PersonalizationActivity.java
@@ -0,0 +1,14 @@
+package com.example.croam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalizationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personalization);
+    }
+}
